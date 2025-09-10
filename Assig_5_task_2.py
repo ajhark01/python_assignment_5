dd24a5d98b6ceb55f1a7eef0342664a4332b2a65
MD AJHARUDDIN <118420367+ajhark01@users.noreply.github.com>
2025-09-10 21:36:02 +0530

    Task 2: Demonstrate List Slicing

---
+++ Assig_5_task_2.py
@@ -0,0 +1,8 @@
+from os.path import split
+
+list1 = [1,2,3,4,5,6,7,8,9,10]
+print ('Original list:',list1)
+first_five = list1[:5]
+print('Extracted first five elements:', first_five)
+first_five.reverse()
+print('Reversed extracted elements:', first_five)
